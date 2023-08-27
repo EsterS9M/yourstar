7da400f4cba9a3e7dc9bbeb25a94817b306e12a6
EsterS9M <aestersantosdev.2001@gmail.com>
2023-08-26 22:22:15 -0300

    Mymoon

---
+++ script.js
@@ -0,0 +1,17 @@
+function Fugir(){
+    var botaoNao = document.getElementById("Nao");
+  
+    botaoNao.style.position = 'absolute';
+
+    var larguraJanela = window.innerWidth;
+    var alturaJanela = window.innerHeight;
+
+    var maxX = larguraJanela - botaoNao.offsetWidth;
+    var maxY = alturaJanela - botaoNao.offsetHeight;
+
+    var aleatorioX = Math.floor(Math.random() * maxX);
+    var aleatorioY = Math.floor(Math.random() * maxY);
+
+    botaoNao.style.left = aleatorioX + "px";
+    botaoNao.style.top = aleatorioY + "px";
+}
